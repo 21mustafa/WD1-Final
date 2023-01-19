e8b30cd383e0964f563f8918b1fd77c5ef53d63b
mostafa asghari <mosti.asghari@gmail.com>
2023-01-19 11:17:45 -0800

    added another section

---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+    "use strict";
+    
+
+    
+   
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+   
+
+
+    
+
+
+
+})(jQuery);
+
