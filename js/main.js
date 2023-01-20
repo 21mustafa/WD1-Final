b7f97580e17d05e8d5c9eb8662508ebfd5acb3cb
mostafa asghari <mosti.asghari@gmail.com>
2023-01-19 21:16:21 -0800

    Add counter js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-(function ($) {
-    "use strict";
-    
+const dataCounts = [14, 146, 85, 221];
 
-    
-   
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
+async function startCounter(countElement, elementNum) {
+  let count = 0;
+  while (count <= elementNum) {
+    countElement.innerHTML = count;
+    count++;
+    await new Promise((r) => setTimeout(r, 25));
+  }
+}
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-   
-
-
-    
-
-
-
-})(jQuery);
-
+(async function () {
+  const countElements = document.getElementsByClassName("counter-back");
+  for (let i = 0; i < countElements.length; i++) {
+    const countElement = countElements[i];
+    let elementNum = dataCounts[i];
+    startCounter(countElement, elementNum);
+  }
+})();
